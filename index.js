369311e65f122bc4446d5e40143d1081952d806b
MwitaChacha <jonahhchacha@gmail.com>
2021-08-28 02:08:49 +0300

    Adding of JavaScript comment

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Scroll effect of the mouse click icon
 $(document).ready(function() {
     $('.mouse-click').click(function() {
       $('html, body, .intro').animate({ scrollTop: $('.about').offset().top}, 1000, 'linear');
