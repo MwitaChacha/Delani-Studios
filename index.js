c228ef90438a3efcfa0944f089e39a232aebd79d
MwitaChacha <jonahhchacha@gmail.com>
2021-08-28 05:09:22 +0300

    Adjusting JavaScript code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Scroll effect of the mouse click icon
-$(document).ready(function() {
-    $('.mouse-click').click(function() {
-      $('html, body, .intro').animate({ scrollTop: $('.about').offset().top}, 1000, 'linear');
-    });
+$(document).ready(function () {
+  $('.mouse-click').click(function () {
+    $('html, body, .intro').animate({
+      scrollTop: $('.about').offset().top
+    }, 1000, 'linear');
   });
+});
